553c898a39a5c7f50f37db892e69b2ae8dc8c99d
Andrew Rymaruk <andrewrymaruk@gmail.com>
2022-09-16 13:15:03 +0300

    Created API with a JWT token

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+app.post("/api/login", (req, res) => {
+  // Mock user
+  const user = {
+    id: 1,
+    username: "brad",
+    email: "brad@gmail.com",
+  };
+
+  jwt.sign({ user }, "secretkey", { expiresIn: "30s" }, (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
+app.post("/api/data", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "Access!",
+        authData,
+      });
+    }
+  });
+});
+
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
+
+app.listen(5000, () => console.log("Server started!"));
